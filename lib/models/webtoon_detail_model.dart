3c43ac6b0fbde725d6c8396d79493a6d230b6c74
Junhyeok Lee <junh0601@naver.com>
2023-04-11 19:16:26 +0900

    ..

--- lib/models/webtoon_detail_model.dart
+++
@@ -1,9 +0,0 @@
-class WebtoonDetailModel {
-  late final title, about, genre, age;
-
-  WebtoonDetailModel.fromJson(Map<String, dynamic> json)
-      : title = json['title'],
-        about = json['about'],
-        genre = json['genre'],
-        age = json['age'];
-}
